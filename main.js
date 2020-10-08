6348dd4918e6520982c2dccf2df8bf47989744ff
nomurashunta <nomurashunta@team-lab.com>
2020-10-08 20:05:37 +0900

    update

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const {registerFont, createCanvas, loadImage} = require('canvas')
 const fs = require('fs');
 const FONT_NAME = 'IPA Gothic'
-registerFont('./resources/font/ipaexg.ttf', { family: FONT_NAME });
+registerFont('./resources/font/ipaexg.ttf', {family: FONT_NAME});
 
 // -----------------参考-----------------------------------------
 // https://blanktar.jp/blog/2020/05/nodejs-write-text-on-image
@@ -13,9 +13,9 @@ registerFont('./resources/font/ipaexg.ttf', { family: FONT_NAME });
 
 generateImages()
 
-async function generateImages(){
-    for (i = 1; i <= 5; i++){
-        let canvas = await createImageCanvas('./resources/images/ot.jpg')
+async function generateImages() {
+    for (i = 1; i <= 5; i++) {
+        let canvas = await createImageCanvas('./resources/images/out.jpg')
         await addTextToCanvas(canvas, `ダミー画像_${i}`)
         await saveCanvasImage(canvas, `./outputs/dummyImage_${i}.jpg`)
     }
@@ -26,20 +26,19 @@ async function createImageCanvas(filePath) {
     const canvas = createCanvas(image.width, image.height)
     const ctx = canvas.getContext('2d')
     ctx.drawImage(image, 0, 0, image.width, image.height);
-    await saveCanvasImage(canvas, '')
     return canvas
 }
 
-async function addTextToCanvas(canvas, text){
+async function addTextToCanvas(canvas, text) {
     const ctx = canvas.getContext('2d')
     ctx.font = `${canvas.width / 10}px ${FONT_NAME}`
     ctx.fillStyle = 'red'
     ctx.fillText(text, 10, canvas.height - canvas.height / 10)
 }
 
-async function saveCanvasImage(canvas, saveFilePath){
+async function saveCanvasImage(canvas, saveFilePath) {
     let base64 = canvas.toDataURL("image/jpeg").replace(/^data:image\/jpeg;base64,/, "")
     fs.writeFile(saveFilePath, base64, 'base64', error => {
-        console.log(`saved : ${saveFilePath}`)
     })
+    console.log(`saved : ${saveFilePath}`)
 }
